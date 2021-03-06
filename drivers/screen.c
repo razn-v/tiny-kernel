7b3006ac7ef55e3a73244171bab81fb472f25787
Raznov <rubysc0@protonmail.com>
2021-03-06 15:41:38 +0100

    Add scroll feature to screen

--- drivers/screen.c
+++ drivers/screen.c
@@ -22,22 +22,47 @@ void screen_clear() {
     }
 }
 
+void swap_line(int y1, int y2) {
+    for (int x = 0; x < VGA_WIDTH; x++) {
+        video_mem[VGA_WIDTH * y1 + x] = video_mem[VGA_WIDTH * y2 + x];
+    }
+}
+
+int get_offset() {
+    return VGA_WIDTH * y_pos + x_pos;
+}
+
+void scroll() {
+    for (int y = 0; y < VGA_HEIGHT - 1; y++) {
+        swap_line(y, y+1);
+    }
+
+    y_pos -= 1;
+    set_cursor_pos(get_offset());
+}
+
 void write_char(char c) {
     if (x_pos >= VGA_WIDTH) {
         x_pos = 0;
         y_pos++;
-    } else if (c == '\n') {
-        x_pos = 0;
-        y_pos++;
-        return;
-    } else {
-        x_pos++;
     }
 
-    int offset = VGA_WIDTH * y_pos + x_pos;
+    if (y_pos >= VGA_HEIGHT) {
+        y_pos = VGA_HEIGHT;
+        scroll();
+    }
 
-    video_mem[offset] = (video_mem[0] & 0xff00) | c;
-    set_cursor_pos(offset);
+    if (c == '\n') {
+        x_pos = 0;
+        y_pos++;
+        set_cursor_pos(get_offset());
+        return;
+    }
+
+    video_mem[get_offset()] = (video_mem[0] & 0xff00) | c;
+
+    x_pos++;
+    set_cursor_pos(get_offset());
 }
 
 void write_str(const char* str) {
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,6 +7,5 @@ void kernel_main() {
     idt_init();
     screen_init();
 
-    write_str("Tiny Kernel.\n");
-    write_str("A tiny kernel made in C for learning purposes.");
+    write_str("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26");
 }
